f3fc1d67e6ce5634e88031ca58b8847e84c59323
knatb <deon.8@hotmail.com>
2021-03-05 16:59:08 -0600

    practica 6 navegacion en mapa

---
+++ Practica 6/main.js
@@ -0,0 +1,63 @@
+var mapa = document.querySelector('#mapa');
+mybutton = document.getElementById("myBtn");
+
+var coordernadas = {
+  lat: 0,
+  lng: 0
+}
+var propiedades = {
+  center: coordernadas,
+  zoom: 20
+}
+
+var icono = {
+  url: 'https://www.gifsanimados.org/data/media/67/coche-y-automovil-imagen-animada-0097.gif',
+  scaledSize: new google.maps.Size(50,50),
+  origin: new google.maps.Point(0,0),
+  anchor: new google.maps.Point(0,0),
+
+}
+
+const marcador = new google.maps.Marker({ 
+  position: coordernadas, 
+  icon: icono
+});
+
+function iniciaMapa() {
+  
+  var map = new google.maps.Map(mapa, propiedades);
+
+  if(navigator.geolocation){
+    movePosition(marcador);
+  }  
+
+  function movePosition(marcador){
+
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log(position);
+
+      var posicion = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+      
+      marcador.setPosition( new google.maps.LatLng(posicion.lat, posicion.lng));
+    });
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
