7061772be9a494a3340d3988496b605e51142b52
knatb <deon.8@hotmail.com>
2021-03-10 20:14:24 -0600

    Practica 6 y 7 (coronavirus)

---
+++ Practica 7/main.js
@@ -0,0 +1,15 @@
+var paises = document.querySelector('#paises');
+
+fetch('data.json')
+.then( function(response){
+    response.json()
+    .then (function(data){
+            data.forEach(registro => {
+                console.log(registro);
+
+                let nombre = document.createElement('p');
+                nombre.textContent = 'País: ' + registro.country + ' - Casos: ' + registro.cases;
+                paises.appendChild(nombre);
+            });
+        });
+});
---
+++ Practica 8/main.js
@@ -0,0 +1,22 @@
+var paises = document.querySelector('#paises');
+
+fetch('https://corona.lmao.ninja/v3/covid-19/countries')
+.then( function(response){
+    response.json()
+    .then (function(data){
+            data.forEach(registro => {
+                
+                let dupla = document.createElement('div');
+                dupla.className = 'row border bg-light';
+                paises.appendChild(dupla);
+
+                let column = document.createElement('div');
+                column.className = 'col-12';
+                paises.appendChild(column);
+
+                let nombre = document.createElement('p');
+                nombre.textContent = 'País: ' + registro.country + ' - Casos: ' + registro.cases;
+                column.appendChild(nombre);
+            });
+        });
+});
