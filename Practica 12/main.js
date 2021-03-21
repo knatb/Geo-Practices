02c37de0790c727f3c79d69da16693321b5f3af4
knatb <deon.8@hotmail.com>
2021-03-20 20:46:44 -0600

    Se agregaron estilos de navbar y footer

--- Practica 12/main.js
+++ Practica 12/main.js
@@ -2,13 +2,16 @@ var coordenadas = {
     lat: -31.563910, lng: 147.154312 
 }
 
-var queryString = window.location.search;
+const queryString = window.location.search;
+
 console.log(queryString);
 
 const urlParams = new URLSearchParams(queryString);
-const idioma = urlParams.get('idioma');
 
+const idioma = urlParams.get('idioma');
 console.log(idioma);
+
+document.getElementById('idioma').value = idioma;
 
 var script = document.createElement('script');
 script.src= 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD6EILSc1qFkatoKwxvk9rKhMnlRwpjvSM&callback=iniciaMapa&language='+idioma;
