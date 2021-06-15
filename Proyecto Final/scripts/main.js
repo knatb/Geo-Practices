60db3b2483e62a8c3a61dfa6fa3e3256d6cb4034
knatb <deon.8@hotmail.com>
2021-06-15 15:19:52 -0500

    Final project card services

--- Proyecto Final/scripts/main.js
+++ Proyecto Final/scripts/main.js
@@ -36,8 +36,9 @@ const listaloggedout = document.querySelectorAll('.logged-out');
             const columna = `
                 <div class="col-12 col-md-4" data-aos="fade-right" data-aos-duration="4000">
                     <img class="img-fluid shadow rounded" id='service' src="./img/services/${service.imagen}" alt="${service.nombre}">
-                    <h2 class="py-3 text-danger">${service.nombre}</h2>
-                    <p class="text-align: justify">${service.descripcion}.00 pesos</p>
+                    <h2 class="py-3">${service.nombre}</h2>
+                    <p class="text-align: justify">${service.descripcion}</p>
+                    <p class="text-danger">$${platillo.precio}.00 pesos</p>
                     <a href="https://paypal.me/knatb08/${service.precio}" target="_blank">
                         <button class="btn btn-primary">Pagar Ahora</button>
                     </a>
